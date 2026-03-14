e5d188e2eeadeff6d36fecfe7b5ef7eb6b9ec75f
mahoneylabs <patsmahoney@gmail.com>
2026-03-14 11:49:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let verbs = []
+let currentVerb
 
+fetch("verbs.json")
+.then(response => response.json())
+.then(data => verbs = data)
+
+function newVerb(){
+ currentVerb = verbs[Math.floor(Math.random()*verbs.length)]
+ document.getElementById("verb").innerText = currentVerb.infinitive
+}
+
+function checkAnswer(){
+ let answer = document.getElementById("answer").value
+
+ if(answer === currentVerb.present){
+   document.getElementById("result").innerText = "Correct!"
+ } else {
+   document.getElementById("result").innerText = "Wrong. Answer: " + currentVerb.present
+ }
+}
